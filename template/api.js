1d55dc8f9917b25bf64163cc355fabd69e4a65c9
marlonsc15 <marlonsantoscorreia@gmail.com>
2022-08-15 13:38:41 -0300

    finalização do HTML E CSS

---
+++ template/api.js
@@ -0,0 +1,6 @@
+// Substitua o CHAVE_DA_API com a chave gerada no site.
+
+export const API_KEY = '8395500b5f2f826139c96542584cefd8';
+export const BASE_URL = 'https://api.themoviedb.org/3/movie/';
+export const IMG_URL = 'https://image.tmdb.org/t/p/w500';
+export const language = 'language=pt-BR';
